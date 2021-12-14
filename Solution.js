13e18b044a50a9c4e02b603f17cda59630a0479a
HrithikSampson <56023811+HrithikSampson@users.noreply.github.com>
2021-12-14 19:02:46 +0530

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,17 +1,19 @@
 // Write your code here
-
-function findMissingDigit(equation){
+class FixEquation{
+    findMissingDigit(equation){
     
-    const s = equation.split('=');
-    for(let i = 0;i<10;i++){
-        //console.log(s[0].replaceAll('?',i));
-        //console.log(s[1].replaceAll('?',i))
-        if(eval(s[0].replaceAll('?',i)) == eval(s[1].replaceAll('?',i))){
-            return i;
+        const s = equation.split('=');
+        for(let i = 0;i<10;i++){
+            //console.log(s[0].replaceAll('?',i));
+            //console.log(s[1].replaceAll('?',i))
+            if(eval(s[0].replaceAll('?',i)) == eval(s[1].replaceAll('?',i))){
+                return i;
+            }
+            
         }
-        
+        return -1;
     }
-    return -1;
 }
 const str = '4? * 47 + 2 = 1976';
-console.log('Answer: ',findMissingDigit(str));
+let f = new FixEquation();
+console.log('Answer: ',f.findMissingDigit(str));
