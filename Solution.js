7d18c10573b54e609294b240ad195a9b751e683f
HrithikSampson <56023811+HrithikSampson@users.noreply.github.com>
2021-12-14 18:47:23 +0530

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,1 +1,17 @@
 // Write your code here
+
+function findMissingDigit(equation){
+    
+    const s = equation.split('=');
+    for(let i = 0;i<10;i++){
+        //console.log(s[0].replaceAll('?',i));
+        //console.log(s[1].replaceAll('?',i))
+        if(eval(s[0].replaceAll('?',i)) == eval(s[1].replaceAll('?',i))){
+            return i;
+        }
+        
+    }
+    return -1;
+}
+const str = '4? * 47 + 2 = 1976';
+console.log('Answer: ',findMissingDigit(str));
